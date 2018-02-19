90e6450872f8050c9727f92fd99b22f76bf6ce59
eric <taochenpfj@hotmail.com>
2018-02-19 23:33:56 +0800

    selectsort

--- src/sort/selectsort.go
+++ src/sort/selectsort.go
@@ -2,30 +2,41 @@ package main
 
 import "fmt"
 
-func SelectSort(arr []int) {
-	leng := len(arr)
+type Sortable interface {
+	Length() int
+	LessThan(int, int) bool
+	Swap(int, int)
+}
+
+func SelectSort(arr Sortable) {
+	leng := arr.Length()
 	for i := 0; i < leng; i++ {
 		minIdx := i
 		for j := i + 1; j < leng; j++ {
-			if arr[minIdx] > arr[j] {
+			if arr.LessThan(j, minIdx) {
 				minIdx = j
 			}
 		}
-		swap(arr, i, minIdx)
+		arr.Swap(i, minIdx)
 	}
 }
 
-func swap(arr []int, i int, j int) {
-	tmp := arr[i]
-	arr[i] = arr[j]
-	arr[j] = tmp
+type IntArray []int
+
+func (arr IntArray) Length() int {
+	return len(arr)
+}
+
+func (arr IntArray) LessThan(i, j int) bool {
+	return arr[i] < arr[j]
+}
+
+func (arr IntArray) Swap(i, j int) {
+	arr[i], arr[j] = arr[j], arr[i]
 }
 
 func main() {
-	arr := []int{5, 4, 3, 2, 1}
+	arr := IntArray{5, 4, 3, 2, 1}
 	SelectSort(arr)
-	for i := 0; i < len(arr); i++ {
-		fmt.Printf("%d,", arr[i])
-	}
-	fmt.Println()
+	fmt.Println(arr)
 }
