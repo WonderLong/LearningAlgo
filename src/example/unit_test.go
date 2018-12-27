2eded763ff198a01db36b07c91e2e790338d15b6
eric <ct@kuaihuoyun.com>
2018-12-27 17:02:57 +0800

    rpc net

--- src/example/unit_test.go
+++ src/example/unit_test.go
@@ -1,10 +1,30 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 type A struct {
 }
 
-func BenchmarkReflection(b *testing.B) {
+func F1() {
+	_ = A{}
+}
 
+func F2(x interface{}) {
+	_ = reflect.ValueOf(x)
+}
+
+func BenchmarkNormal(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		F1()
+	}
+}
+
+func BenchmarkReflection(b *testing.B) {
+	var x A
+	for i := 0; i < b.N; i++ {
+		F2(x)
+	}
 }
